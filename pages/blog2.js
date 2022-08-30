16a279c3f5ecf32051f67c98518e2b04d271253e
alindrapu <alindrapu@gmail.com>
2022-08-31 02:41:39 +0700

    mainbanner & navbar

--- pages/blog2.js
+++ pages/blog2.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import Navbar from '../components/Layouts/Navbar';
-import PageBanner from '../components/Common/PageBanner';
-import BlogCardTwo from '../components/Blog/BlogCardTwo';
-import Footer from '../components/Layouts/Footer';
+import React, { Component } from "react";
+import Navbar from "../components/Layouts/Navbar";
+import PageBanner from "../components/Common/PageBanner";
+import BlogCardTwo from "../components/Blog/BlogCardTwo";
+import Footer from "../components/Layouts/Footer";
 
 class Blog2 extends Component {
-    render() {
-        return (
-            <>
-                <Navbar />
+  render() {
+    return (
+      <>
+        <Navbar />
 
-                <PageBanner
-                    pageTitle="Blog Right Sidebar" 
-                    breadcrumbTextOne="Home" 
-                    breadcrumbTextTwo="Blog Right Sidebar" 
-                    breadcrumbUrl="/" 
-                    bgImage="banner-bg-img4"
-                />
+        <PageBanner
+          pageTitle="Blog Right Sidebar"
+          breadcrumbTextOne="Home"
+          breadcrumbTextTwo="Blog Right Sidebar"
+          breadcrumbUrl="/"
+          bgImage="banner-bg-img4"
+        />
 
-                <BlogCardTwo />
-                
-                <Footer />
-            </>
-        );
-    }
+        <BlogCardTwo />
+
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default Blog2;
