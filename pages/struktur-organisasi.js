56cb9d09674f04710229048112d095c6bd55d8a9
alindrapu <alindrapu@gmail.com>
2022-09-06 15:36:18 +0700

    dasar hukum download button

--- pages/struktur-organisasi.js
+++ pages/struktur-organisasi.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "../components/Layouts/Navbar";
 import PageBanner from "../components/Common/PageBanner";
-import TentangKamiContent from "../components/AboutUs/TentangKamiContent";
-import FunFacts from "../components/Common/FunFacts";
-import GetStartedProject from "../components/Common/GetStartedProject";
-import Testimonial from "../components/Common/Testimonial";
-import TeamMember from "../components/Common/TeamMember";
-import Customers from "../components/Common/Customers";
 import Footer from "../components/Layouts/Footer";
-import Linimasa from "../components/AboutUs/Linimasa";
 import StrukturOrganisasiContent from "../components/AboutUs/StrukturOrganisasiContent";
 
 class StrukturOrganisasi extends Component {
@@ -26,23 +19,7 @@ class StrukturOrganisasi extends Component {
           bgImage="banner-bg-img1"
         />
 
-        {/* <TentangKamiContent />
-
-        <Linimasa /> */}
-
         <StrukturOrganisasiContent />
-
-        {/* <div className="pb-100">
-          <GetStartedProject />
-        </div>
-
-        <FunFacts /> */}
-
-        {/* <Testimonial /> */}
-
-        {/* <TeamMember /> */}
-
-        {/* <Customers /> */}
 
         <Footer />
       </>
